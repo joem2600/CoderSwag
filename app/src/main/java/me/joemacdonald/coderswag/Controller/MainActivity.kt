0cfabebae8751dedb563f4541c6fd99270ec420a
JoeM <joem2525@gmail.com>
2017-12-10 15:50:10 -0500

    App fails to launch due "getFont()" exception

--- app/src/main/java/me/joemacdonald/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/me/joemacdonald/coderswag/Controller/MainActivity.kt
@@ -2,23 +2,28 @@ package me.joemacdonald.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import me.joemacdonald.coderswag.Adapters.CategoryAdapter
+import me.joemacdonald.coderswag.Adapters.CategoryRecycleAdapter
 import me.joemacdonald.coderswag.R
 import me.joemacdonald.coderswag.Services.DataService
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
-
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
+
 
 
     }
